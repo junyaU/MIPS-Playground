276434d4f21554c8c45921bf7910f1425e019c85
junyaU <junya19991215@icloud.com>
2024-04-29 18:02:49 +0900

    Refactor sort algorithm in sort.asm and sort.c

--- sort.asm
+++ sort.asm
@@ -3,8 +3,25 @@ for1:
     move $s0, $zero
     slt $t0, $s0, $a1
     beq $t0, $zero, exit1
-    ; do something
+for2:
+    addi $s0, $s0, -1
+    slti $t0, $s1, 0
+    bne $t0, $zero, exit2
+    sll $t1, $s1, 2
+    add $t2, $t1, $a0
+    lw $t3, 0($t1)
+    lw $t4, 4($t1)
+    slt $t0, $t4, $t3
+    beq $t0, $zero, exit2
+    move $a0, $s2
+    move $a1, $s1
+    jal swap
+    add $s1, $s1, -1
+    j for2
+exit2:
     addi $s0, $s0, 1
     j for1
 
 exit1:
+    ; restore registers
+    jr $ra
--- sort.c
+++ sort.c
@@ -1,7 +1,7 @@
 void sort(int v[], int n) {
   int i, j;
   for (i = 0; i < n; i++) {
-    for (j = i; j > 0 && v[j] < v[j - 1]; j--) {
+    for (j = i - 1; j >= 0 && v[j] > v[j + 1]; j--) {
       swap(v, j);
     }
   }
