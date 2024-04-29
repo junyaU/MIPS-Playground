58bec5b39482a13fe1d0a3dbd2de2ab5790a3c97
junyaU <junya19991215@icloud.com>
2024-04-29 16:59:48 +0900

    Refactor swap function to use MIPS assembly in swap.asm and swap.c

--- swap.asm
+++ swap.asm
@@ -1,1 +1,12 @@
 swap:
+    add $t0, $t0, $zero ; initialize temp to 0
+    sll $t1, $a1, 2 ; t1 = i
+    add $t1, $t1, $a0 ; t1 = A + i
+
+    lw $t0, 0($t1) ; temp = A[i]
+    lw $t2, 4($t1) ; t2 = A[i + 1]
+
+    sw $t2, 0($t1) ; A[i] = A[i + 1]
+    sw $t0, 4($t1) ; A[i + 1] = temp
+
+    jr $ra ; return
