fca6265fb3a6c1f81d86c76a48029fac547833b7
junyaU <junyaadgj@gmail.com>
2024-05-03 15:49:25 +0900

    Refactor loop in while.asm

--- while.asm
+++ while.asm
@@ -1,4 +1,3 @@
-# 元のコード
 Loop:
     sll $t1, $s3, 2
     add $t1, $t1, $s6
@@ -7,3 +6,11 @@ Loop:
     addi $s3, $s3, 1
     j Loop
 Exit:
+
+Loop2:
+    sll $t1, $s3, 2
+    add $t1, $t1, $s6
+    lw $t0, 0($t1)
+    addi $s3, $s3, 1
+    beq $t0, $s5, Loop2
+Exit2:
